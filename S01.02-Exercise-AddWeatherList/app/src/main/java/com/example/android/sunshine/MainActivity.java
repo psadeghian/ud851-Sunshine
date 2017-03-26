50e3f4bb5ce1ac619bd4cdce83e86c7bc0364123
Pasha Sadeghian <Pasha@Pashas-Mac.local>
2017-03-26 03:11:05 -0400

    Finished S0102

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,20 +17,36 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a field to store the weather display TextView
+    // COMPLETED (1) Create a field to store the weather display TextView
+    private TextView mWeatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        // TODO (2) Use findViewById to get a reference to the weather display TextView
+        // COMPLETED (2) Use findViewById to get a reference to the weather display TextView
+        mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
 
-        // TODO (3) Create an array of Strings that contain fake weather data
 
-        // TODO (4) Append each String from the fake weather data array to the TextView
+        // COMPLETED (3) Create an array of Strings that contain fake weather data
+        String[] fakeWeatherData = {
+            "Today, May 17 - Clear - 17°C / 15°C",
+            "Tomorrow - Cloudy - 19°C / 15°C",
+            "Thursday - Rainy- 30°C / 11°C",
+            "Friday - Thunderstorms - 21°C / 9°C",
+            "Saturday - Thunderstorms - 16°C / 7°C",
+            "Sunday - Rainy - 16°C / 8°C",
+            "Monday - Partly Cloudy - 15°C / 10°C",
+        };
+
+        // COMPLETED (4) Append each String from the fake weather data array to the TextView
+        for (int i = 0; i < fakeWeatherData.length; i++) {
+            mWeatherTextView.append(fakeWeatherData[i] + "\n");
+        }
     }
 }
